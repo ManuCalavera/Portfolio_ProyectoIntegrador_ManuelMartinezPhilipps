55dd28643bca6c33dfcbd8f5528e52c8eeb34dfc
ManuCalavera <111749443+ManuCalavera@users.noreply.github.com>
2022-08-22 11:37:48 -0300

    header

--- PI_MMP/src/app/app.module.ts
+++ PI_MMP/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BanerComponent } from './components/baner/baner.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BanerComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

e2fd776b8906de77c1e9333ae49b1760212bbbdb
ManuCalavera <111749443+ManuCalavera@users.noreply.github.com>
2022-08-25 02:46:57 -0300

    Creación del Backend

---
+++ PI_MMP/Backend/mmp/src/main/java/com/portfolio/mmp/MmpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmpApplication.class, args);
+	}
+
+}
---
+++ PI_MMP/Backend/mmp/src/main/java/com/portfolio/mmp/repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.mmp.Repository;
+
+import com.portfolio.mmp.entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
